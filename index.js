0843bfacd9361dfd9e3ea69930621a5349ad1d32
wickedev <orange881217@gmail.com>
2020-01-05 19:42:35 +0900

    autoprefixer: browser -> browserslist

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const defaultOptions = {
     plugins: [
       PostCssFlexBugFixes,
       autoprefixer({
-        browsers: ['>1%', 'last 4 versions', 'Firefox ESR', 'not ie < 9'],
+        browserslist: ['>1%', 'last 4 versions', 'Firefox ESR', 'not ie < 9'],
         flexbox: 'no-2009',
       }),
     ],
